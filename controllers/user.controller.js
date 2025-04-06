7849156acea36bec315d34b961c54204dd6f9571
harshalDev <harshalwarade344@gmail.com>
2025-04-06 16:24:37 +0530

    resolve login issue

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -92,8 +92,9 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
-        httpsOnly: true,
-        sameSite: "strict",
+        httpOnly: true,
+        securer: true,
+        sameSite: "None",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
